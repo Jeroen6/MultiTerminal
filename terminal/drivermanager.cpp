5f6c63dd06b4a0c83162940f6b21fc4adb2d2a1e
Jeroen Lodder <lodderj@gmail.com>
2014-03-22 23:05:47 +0100

    add license

--- terminal/drivermanager.cpp
+++ terminal/drivermanager.cpp
@@ -1,3 +1,18 @@
+/* MultiTerminal (TCP or Serial terminal emulator with filtering capabilites)
+ * Copyright (C) 2014  Jeroen Lodder
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ */
 #include <Qt>
 #include <QObject>
 #include <QProcess>
